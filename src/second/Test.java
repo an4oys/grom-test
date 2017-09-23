608153dd5a26afbb1c30ef9dd7065d093e8632f5
an4oys <ann.lytvynova@yahoo.com>
2017-09-24 02:17:15 +0300

    test commit

---
+++ src/second/Test.java
@@ -0,0 +1,10 @@
+package second;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My first Java program....");
+
+        System.out.printf("Done");
+    }
+}
+
---
+++ src/third/Test.java
@@ -0,0 +1,9 @@
+package third;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Here is more complex");
+
+        System.out.println("text");
+    }
+}
